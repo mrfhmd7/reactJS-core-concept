0ac7b5da439d484196893ac4c47ea19728432dbf
mrfhmd7 <mrf.hmd7@gmail.com>
2023-12-22 21:16:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const singer1 = { name: 'Mr. X', job: 'singing' };
+const singer2 = { name: 'Mrs. X', job: 'singing' };
+
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white',
+  padding: '5px',
+  borderRadius: '5px',
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div className="welcome">
+        <h2>This is my first React App project.</h2>
+        </div>
+        <div className="music">
+          <p style={singerStyle}>Name: {singer1.name} is a good in { singer1.job}.</p>
+          <p style={{color: 'purple',backgroundColor: 'white',padding: '5px',borderRadius: '5px'}}>Name: {singer2.name} is a good at { singer2.job}.</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
