65a87f57d1fdff0be900d7b74ca8e2c9234a41ad
mrfhmd7 <mrf.hmd7@gmail.com>
2023-12-23 10:18:45 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-const singer1 = { name: 'Mr. X', job: 'singing' };
-const singer2 = { name: 'Mrs. X', job: 'singing' };
+// const singer1 = { name: 'Mr. X', job: 'singing' };
+// const singer2 = { name: 'Mrs. X', job: 'singing' };
+const professions = [
+  { name: 'Ashikur Rahman', job: 'Data science' },
+  { name: 'Soubj Ahmed', job: 'Web development' },
+  { name: 'Nafiz', job: 'Chemist' },
+  { name: 'Imran', job: 'Immigrant'}
+]
 
 const singerStyle = {
   color: 'purple',
@@ -12,6 +18,7 @@ const singerStyle = {
 }
 
 function App() {
+  const friends = ['Ashikur Rahman', 'Sobuj Ahmed', 'Nafiz', 'Imran']
   return (
     <div className="App">
         <div className="welcome">
@@ -21,28 +28,30 @@ function App() {
           <p style={singerStyle}>Name: {singer1.name} is a good in { singer1.job}.</p>
           <p style={{color: 'purple',backgroundColor: 'white',padding: '5px',borderRadius: '5px'}}>Name: {singer2.name} is a good at { singer2.job}.</p>
         </div> */}
-      <Mine></Mine>
-      <Friend></Friend>
-      <Mine></Mine>
-      <Friend></Friend>
+      <Mine name='Maruf Ahmed' job='Web Development'></Mine>
+      {professions.map(profession => <Friend name={profession.name} job={profession.job}></Friend>)}
+      {/* <Friend name='Ashikur Rahman' job='Data Science'></Friend> */}
+      <Mine name='Maruf Ahmed' job='Cyber Security'></Mine>
+      {friends.map(friend => <Friend name={friend} ></Friend>)}
+      <Friend name='Sobuj Ahmed' job='Web Development'></Friend>
     </div>
   );
 }
 
-function Mine() { 
+function Mine(props) { 
   return (
     <div className='mine'>
-      <h2>Name: Maruf Ahmed</h2>
-      <h3>Profession: Web development</h3>
+      <h2>Name: { props.name}</h2>
+      <h3>Profession: {props.job}</h3>
     </div>
   )
 }
 
-function Friend() { 
+function Friend(props) { 
   return (
     <div className='friend'>
-      <h2>Name: Mr. X</h2>
-      <h3>Profession: Data Science/Web development</h3>
+      <h2>Name: { props.name}</h2>
+      <h3>Profession: { props.job}</h3>
     </div>
   )  
 }
